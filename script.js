43d0f1fb875a6844ba4a152c58d5b7186e185990
Casey DeBoth <casey.deboth@icloud.com>
2025-07-02 09:53:00 -0500

    Merged fetches with promise, added elevation chart and click popups

--- script.js
+++ script.js
@@ -11,20 +11,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //   .bindPopup('Monkey Manor')
 //   .openPopup();
 
-
-fetch('routes/tracks.geojson')
-    .then(res => res.json())
-    .then(data => {
-        L.geoJSON(data, {
-            style: { color: 'blue', weight: 4 },
-            // onEachFeature: (feature, layer) => {
-            //     const name = feature.properties.name || "Unnamed Route";
-            //     const time = feature.properties.time;
-            //     layer.bindPopup(`<b>${name}</b><br>${time}`);
-            // }
-            
-        }).addTo(map);
-    });
+function distanceSq(a,b) {
+            return (a.lat - b.lat) ** 2 + (a.lon - b.lng) ** 2;
+        }
 
 function haversine(p1, p2) {
     const R = 6371; //Earth radius in km
@@ -35,127 +24,141 @@ function haversine(p1, p2) {
     return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); // in km
 }
 
-fetch('routes/track_points.geojson')
-    .then(res => res.json())
-    .then(pointsData => {
-        pointsData.features.forEach((feature, index) => {
-            if (index % 100 === 0) { // only every 100th point
-                const [lon, lat] = feature.geometry.coordinates;
-                const elevation = feature.properties.ele;
-                const time = feature.properties.time;
-                const hr = extractFromExtension(feature.properties.gpxtpx_TrackPointExtension, 'hr');
+function extractFromExtension(xmlStr, tag) {
+        const match = xmlStr.match(new RegExp(`<gpxtpx:${tag}>(\\d+)</gpxtpx:${tag}>`));
+        return match ? match[1]: null;
+}
 
-                L.circleMarker([lat, lon] ,{
-                    radius: 4,
-                    color: 'red',
-                    fillOpacity: 0.7
-                }).addTo(map).bindPopup(`
-                    <b>Elevation:</b> ${(elevation * 3.28).toFixed(0)} ft<br>
-                    <b>Time:</b> ${new Date(time).toLocaleTimeString()}<br>
-                    <b>Heart Rate:</b> ${hr || 'n/a'} bpm
-                    `);
-            }
-        });
 
-        
-        const coords = pointsData.features.map(f => {
+Promise.all([
+    fetch('routes/tracks.geojson').then(res => res.json()),
+    fetch('routes/track_points.geojson').then(res => res.json())
+]).then(([trackData, pointData]) => {
+    const polyline = L.geoJSON(trackData, {
+            style: { color: 'blue', weight: 4 }            
+        }).addTo(map);
+
+    const coords = pointData.features.map(f => {
             const [lon, lat] = f.geometry.coordinates;
             const ele = f.properties.ele; //* 3.28
             const time = new Date(f.properties.time);
-            return { lat, lon, ele, time};
+            const hr = extractFromExtension(f.properties.gpxtpx_TrackPointExtension || '', 'hr');
+            return { lat, lon, ele, time, hr};
         });
 
-        // Center map on route
-        const latlngs = coords.map(c => [c.lat, c.lon]);
+
+    // Mile Markers
+    let dist = 0;
+    let mileCount = 1;
+
+    for()
+    
+    const latlngs = coords.map(c => [c.lat, c.lon]);
         map.fitBounds(latlngs);
 
-        // Building elevation chart
-        let distance = 0;
-        const elevationData = [];
-        const labels = [];
+    const popup = L.popup();
 
-        for (let i = 1; i < coords.length; i++) {
-            const prev = coords[i - 1];
-            const curr = coords[i];
+    polyline.on('click', function(e) {
+            const latlng = e.latlng;
 
-            // add distance between points
-            distance += haversine(prev, curr);
+            // find nearest coordinate
+            let nearest = coords.reduce((a, b) =>
+                distanceSq(a, latlng) < distanceSq(b, latlng) ? a : b
+            );
 
-            // Add elevation point
-            elevationData.push(curr.ele * 3.28084); // convert m to ft
-            labels.push((distance / 1.609).toFixed(2)); // distance in mi
-        }
+            const elevationFt = (nearest.ele * 3.28084).toFixed(0);
+            const timeStr = nearest.time.toLocaleTimeString();
+            const hr = nearest.hr;
+            const popupHtml = `
+                <b>Elevation:</b> ${elevationFt} ft<br>
+                <b>Time:</b> ${timeStr} <br>
+                <b>Heart Rate:</b> ${hr || 'n/a'} bpm
+                `;
 
-        const ctx = document.getElementById('elevationChart').getContext('2d');
-        new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Elevation (ft)',
-                    data: elevationData,
-                    fill: true,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    tension: 0.3,
-                    pointRadius: 0,
-                }]
-            },
-            options: {
-                scales: {
-                    x: {
-                        title: {
-                            display: true,
-                            text: 'Distance (mi)'
-                        },
-                        ticks: {
-                            maxTicksLimit: 10
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: 'Elevation (ft)'
-                        }
-                    }
-                },
-                plugins: {
-                    legend: {
-                        display: false
-                    }
-                }
-            }
-        });
-
-        let totalDist = 0;
-        let totalElevGain = 0;
-        for (let i = 1; i < coords.length; i++) {
-            const prev = coords[i-1];
-            const curr = coords[i];
-
-            totalDist += haversine(coords[i-1],coords[i]);
-
-            const gain = coords[i].ele - coords[i - 1].ele;
-
-            if (!isNaN(gain) && gain > 0) { // ~2 inches
-                totalElevGain += gain;
-            }
-        }
-
-        const totalTimeSec = (coords.at(-1).time - coords[0].time) / 1000;
-        const avgPace = totalTimeSec / 60 / (totalDist / 1.609);
-        const paceMin = Math.floor(avgPace);
-        const paceDec = avgPace - paceMin;
-        const paceSec = paceDec * 60;
-
-
-        document.getElementById("distance").textContent = (totalDist / 1.609).toFixed(2);
-        document.getElementById("elevation").textContent = Math.round(totalElevGain *  3.28);
-        document.getElementById("pace").textContent = paceMin + ":" + paceSec.toFixed(0);
+            popup.setLatLng(latlng).setContent(popupHtml).openOn(map);
     });
 
-    function extractFromExtension(xmlStr, tag) {
-        const match = xmlStr.match(new RegExp(`<gpxtpx:${tag}>(\\d+)</gpxtpx:${tag}>`));
-        return match ? match[1]: null;
+    // Building elevation chart
+    let distance = 0;
+    const elevationData = [];
+    const labels = [];
+
+    for (let i = 1; i < coords.length; i++) {
+        const prev = coords[i - 1];
+        const curr = coords[i];
+
+        // add distance between points
+        distance += haversine(prev, curr);
+
+        // Add elevation point
+        elevationData.push(curr.ele * 3.28084); // convert m to ft
+        labels.push((distance / 1.609).toFixed(2)); // distance in mi
     }
 
+    const ctx = document.getElementById('elevationChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Elevation (ft)',
+                data: elevationData,
+                fill: true,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                tension: 0.3,
+                pointRadius: 0,
+            }]
+        },
+        options: {
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Distance (mi)'
+                    },
+                    ticks: {
+                        maxTicksLimit: 10
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Elevation (ft)'
+                    }
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+
+
+    let totalDist = 0;
+    let totalElevGain = 0;
+    for (let i = 1; i < coords.length; i++) {
+        const prev = coords[i-1];
+        const curr = coords[i];
+
+        totalDist += haversine(coords[i-1],coords[i]);
+        const gain = coords[i].ele - coords[i - 1].ele;
+
+        if (!isNaN(gain) && gain > 0) { // ~2 inches
+            totalElevGain += gain;
+        }
+    }
+
+    const totalTimeSec = (coords.at(-1).time - coords[0].time) / 1000;
+    const avgPace = totalTimeSec / 60 / (totalDist / 1.609);
+    const paceMin = Math.floor(avgPace);
+    const paceDec = avgPace - paceMin;
+    const paceSec = paceDec * 60;
+
+
+    document.getElementById("distance").textContent = (totalDist / 1.609).toFixed(2);
+    document.getElementById("elevation").textContent = Math.round(totalElevGain *  3.28);
+    document.getElementById("pace").textContent =  `${paceMin}:${paceSec.toFixed(0).padStart(2, '0')}`;
+})
