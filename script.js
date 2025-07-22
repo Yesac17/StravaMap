e8cecca0c454763cb303b887f9805911a3dc8a67
Casey DeBoth <casey.deboth@icloud.com>
2025-07-21 20:24:42 -0500

    added mouse over tracking on map

--- script.js
+++ script.js
@@ -44,98 +44,67 @@ Promise.all([
             const time = new Date(f.properties.time);
             const hr = extractFromExtension(f.properties.gpxtpx_TrackPointExtension || '', 'hr');
             return { lat, lon, ele, time, hr};
-        });
+    });
+    let cumDist = 0;
+    for (let i = 0; i < coords.length; i++) {
+                if(i > 0) {
+                    cumDist += haversine(coords[i-1], coords[i]) / 1.609;
+                }
+                coords[i].distanceMi = cumDist;
+    }
 
+    const hoverMarker = L.circleMarker([0,0], {
+        radius: 7,
+        color: 'orange',
+        fillColor: 'yellow',
+        fillOpacity: 0.8,
+    }).addTo(map).setStyle({ opacity: 0, fillOpacity: 0 });
 
-    // Mile Markers
-    let dist = 0;
-    let mileCount = 1;
+    function handleTooltipSync(sourceChart, targetChart, coords) {
+        return function(context) {
+            const tooltip = context.tooltip;
 
-    for()
-    
-    const latlngs = coords.map(c => [c.lat, c.lon]);
-        map.fitBounds(latlngs);
+            if (tooltip.opacity === 0) {
+                hoverMarker.setStyle({ opacity: 0, fillOpacity: 0 });
+                hoverMarker.closePopup();
+                targetChart.setActiveElements([]);
+                targetChart.tooltip.setActiveElements([], {x: 0, y: 0});
+                targetChart.update();
+                return;
+            }
 
-    const popup = L.popup();
+            const hoveredDist = parseFloat(tooltip.dataPoints[0].label);
 
-    polyline.on('click', function(e) {
-            const latlng = e.latlng;
-
-            // find nearest coordinate
-            let nearest = coords.reduce((a, b) =>
-                distanceSq(a, latlng) < distanceSq(b, latlng) ? a : b
+            let nearest = coords.reduce((a,b) =>
+                Math.abs((a.distanceMi || 0) - hoveredDist) < Math.abs((b.distanceMi || 0) - hoveredDist) ? a : b
             );
+
+            hoverMarker.setLatLng([nearest.lat, nearest.lon]);
+            hoverMarker.setStyle({ opacity: 1, fillOpacity: 0.8});
 
             const elevationFt = (nearest.ele * 3.28084).toFixed(0);
             const timeStr = nearest.time.toLocaleTimeString();
             const hr = nearest.hr;
-            const popupHtml = `
+
+            hoverMarker.bindPopup(`
                 <b>Elevation:</b> ${elevationFt} ft<br>
                 <b>Time:</b> ${timeStr} <br>
-                <b>Heart Rate:</b> ${hr || 'n/a'} bpm
-                `;
+                <b>Heart Rate:</b> ${hr || 'n/a'} bpm    
+            `).openPopup();
 
-            popup.setLatLng(latlng).setContent(popupHtml).openOn(map);
-    });
+            const matchIndex = targetChart.data.labels.findIndex(l => 
+                Math.abs(parseFloat(1) - hoveredDist) < 0.01
+            );
 
-    // Building elevation chart
-    let distance = 0;
-    const elevationData = [];
-    const labels = [];
+            if (matchIndex === -1) return;
 
-    for (let i = 1; i < coords.length; i++) {
-        const prev = coords[i - 1];
-        const curr = coords[i];
-
-        // add distance between points
-        distance += haversine(prev, curr);
-
-        // Add elevation point
-        elevationData.push(curr.ele * 3.28084); // convert m to ft
-        labels.push((distance / 1.609).toFixed(2)); // distance in mi
-    }
-
-    const ctx = document.getElementById('elevationChart').getContext('2d');
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Elevation (ft)',
-                data: elevationData,
-                fill: true,
-                borderColor: 'rgba(75, 192, 192, 1)',
-                backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                tension: 0.3,
-                pointRadius: 0,
-            }]
-        },
-        options: {
-            scales: {
-                x: {
-                    title: {
-                        display: true,
-                        text: 'Distance (mi)'
-                    },
-                    ticks: {
-                        maxTicksLimit: 10
-                    }
-                },
-                y: {
-                    title: {
-                        display: true,
-                        text: 'Elevation (ft)'
-                    }
-                }
-            },
-            plugins: {
-                legend: {
-                    display: false
-                }
+            if (matchIndex !=- -1) {
+                targetChart.setActiveElements([{datasetIndex: 0, index: matchIndex }]);
+                targetChart.tooltip.setActiveElements([{ datasetIndex: 0, index: matchIndex }]);
+                targetChart.update();
             }
         }
-    });
-
+    }
 
     let totalDist = 0;
     let totalElevGain = 0;
@@ -161,4 +130,257 @@ Promise.all([
     document.getElementById("distance").textContent = (totalDist / 1.609).toFixed(2);
     document.getElementById("elevation").textContent = Math.round(totalElevGain *  3.28);
     document.getElementById("pace").textContent =  `${paceMin}:${paceSec.toFixed(0).padStart(2, '0')}`;
+
+    const popup = L.popup();
+
+    polyline.on('click', function(e) {
+            const latlng = e.latlng;
+
+            // find nearest coordinate
+            let nearest = coords.reduce((a, b) =>
+                distanceSq(a, latlng) < distanceSq(b, latlng) ? a : b
+            );
+
+            const elevationFt = (nearest.ele * 3.28084).toFixed(0);
+            const timeStr = nearest.time.toLocaleTimeString();
+            const hr = nearest.hr;
+            const popupHtml = `
+                <b>Elevation:</b> ${elevationFt} ft<br>
+                <b>Time:</b> ${timeStr} <br>
+                <b>Heart Rate:</b> ${hr || 'n/a'} bpm
+                `;
+
+            popup.setLatLng(latlng).setContent(popupHtml).openOn(map);
+    });
+
+    // Mile Markers
+    let dist = 0;
+    let mileCount = 1;
+    let lastMileTime = coords[0].time;
+
+    for(let i = 1; i < coords.length; i++) {
+        const prev = coords[i-1];
+        const curr = coords[i];
+        dist += haversine(prev,curr); //km
+
+        if (dist >= mileCount * 1.60934) { //every mile
+            const elapsed = (curr.time - lastMileTime) / 1000;
+            lastMileTime = curr.time;
+
+            const paceMin = Math.floor(elapsed / 60);
+            const paceSec = Math.round(elapsed % 60);
+            const paceStr = `${paceMin}:${paceSec.toString().padStart(2, '0')}`;
+
+            L.marker([curr.lat, curr.lon], {
+                icon: L.divIcon({
+                    className: 'mile-marker',
+                    html: `<div class="mile-label">${mileCount}</div>`,
+                    iconSize: [24, 24],
+                    iconAnchor: [12, 12]
+                })
+            }).addTo(map).bindPopup(`
+                <b>Mile ${mileCount}: ${paceStr}</b>
+                `)
+
+            mileCount++;
+        }
+    }
+    
+    polyline.bringToFront();
+
+    const latlngs = coords.map(c => [c.lat, c.lon]);
+        map.fitBounds(latlngs);
+
+
+    const crosshairPlugin = {
+        id: 'crosshairLine',
+        afterDatasetsDraw: function(chart, args, options) {
+            const {ctx, tooltip, chartArea} = chart;
+            // console.log("Tooltip:", tooltip);
+            if (chart.tooltip && chart.tooltip._active && chart.tooltip._active.length) {
+                const x = tooltip._active[0].element.x;
+                ctx.save();
+                ctx.beginPath();
+                ctx.moveTo(x, chartArea.top);
+                ctx.lineTo(x, chartArea.bottom);
+                ctx.lineWidth = 1;
+                ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+                ctx.stroke();
+                ctx.restore();
+            }
+        }
+    };
+
+    // Building elevation chart
+    let distance = 0;
+    const elevationData = [];
+    const labels = [];
+
+    for (let i = 1; i < coords.length; i++) {
+        const prev = coords[i - 1];
+        const curr = coords[i];
+
+        // add distance between points
+        distance += haversine(prev, curr);
+
+        // Add elevation point
+        elevationData.push(curr.ele * 3.28084); // convert m to ft
+        labels.push((distance / 1.609).toFixed(2)); // distance in mi
+    }
+
+    const ctx = document.getElementById('elevationChart').getContext('2d');
+    const elevationChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Elevation (ft)',
+                data: elevationData,
+                fill: true,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                tension: 0.3,
+                pointRadius: 0,
+                pointHoverRadius: 8,
+            }]
+        },
+        options: {
+            interaction: {
+                mode: 'index',
+                intersect: false
+            },
+            plugins: {
+                legend: {display: false},
+                crosshairLine: true
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Distance (mi)'
+                    },
+                    type: 'linear',
+                    min: 0,
+                    max: Math.round(totalDist / 1.609), 
+                    ticks: {
+                        stepSize: 1,  // <-- show ticks at every 1 unit (e.g., every whole mile)
+                        beginAtZero: true,
+                    }
+                },
+                y: {
+                    title: {
+                        display: true,
+                        text: 'Elevation (ft)'
+                    }
+                }
+            }
+        },
+        plugins: [crosshairPlugin]
+    });
+   
+
+    //Building Pace Chart
+
+    const paceData = [];
+    const paceLabels = [];
+
+    let cumulativeDist = 0;
+
+    for(let i = 1; i < coords.length; i++) {
+        const prev = coords[i-1];
+        const curr = coords[i];
+        
+        const segmentDistKm = haversine(prev,curr)
+        cumulativeDist += segmentDistKm;
+
+        const timeDiffSec = (curr.time - prev.time) / 1000;
+
+        const paceMinPerKm = (timeDiffSec / 60) / segmentDistKm;
+        const paceMinPerMi = paceMinPerKm * 1.609;
+
+        if (paceMinPerMi < 20 && paceMinPerMi > 3) {
+            paceData.push(paceMinPerMi);
+            paceLabels.push((cumulativeDist / 1.609).toFixed(2));
+        }
+    }
+
+    const smoothedPace =[];
+    const timeWindow = 5;
+
+    for(let i = 1; i < paceData.length; i++) {
+        let sum = 0;
+        let count = 0;
+        let baseTime = coords[i+1]?.time;
+
+        for (let j = i; j >= 0; j--) {
+            if (!baseTime || (baseTime - coords[j].time) /1000 > timeWindow) break; 
+            
+            sum += paceData[j];
+            count++;
+        }
+
+        smoothedPace.push(sum / count);
+    }
+
+
+    const ctx2 = document.getElementById('paceChart').getContext('2d');
+    const paceChart = new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: paceLabels,
+            datasets: [{
+                label: 'Pace (min/mi)',
+                data: smoothedPace,
+                fill: false,
+                borderColor: 'orange',
+                backgroundColor: 'orange',
+                borderWidth: 1.5,
+                tension: 0.3,
+                pointRadius: 0,
+            }]
+        },
+        options: {
+            interaction: {
+                mode: 'index',
+                intersect: false
+            },
+            plugins: {
+                legend: {display: false},
+                crosshairLine: true
+            },
+            scales: {
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Distance (mi)'
+                    },
+                    type: 'linear',
+                    min: 0,
+                    // max: Math.round(totalDist / 1.609), 
+                    ticks: {
+                        stepSize: 1,  // <-- show ticks at every 1 unit (e.g., every whole mile)
+                        beginAtZero: true,
+                    },
+                    grid: {
+                        drawTicks: true,
+                        drawOnChartArea: true
+                    }
+                },
+                y: {
+                    min: 6,
+                    max: 10,
+                    reverse: true,
+                    title: {
+                        display: true,
+                        text: 'Pace (min/mi)'
+                    }
+                }
+            },
+            backgroundColor: 'white',
+        },
+        plugins: [crosshairPlugin]
+    });
+
+    elevationChart.options.plugins.tooltip.external = handleTooltipSync(elevationChart, paceChart, coords);
+    paceChart.options.plugins.tooltip.external = handleTooltipSync(paceChart, elevationChart, coords);
 })
